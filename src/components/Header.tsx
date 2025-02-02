b086ea6e3c78f986f1c519cb0f4a0b7a73ec4442
ravkun <sp810111@gmail.com>
2025-02-02 16:34:11 +0530

    fixed a bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -144,12 +144,12 @@ const Header = () => {
                 closed: {
                   opacity: 0,
                   height: 0,
-                  transition: { duration: 0.4, ease: "easeInOut" },
+                  transition: { duration: 0.2, ease: "easeInOut" },
                 },
                 open: {
                   opacity: 1,
-                  height: "auto",
-                  transition: { duration: 0.4, ease: "easeInOut" },
+                  height: "300px",
+                  transition: { duration: 0.2, ease: "easeInOut" },
                 },
               }}
             >
